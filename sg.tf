2f881fe87fa0cb9647d722bc2c267fd908e3a634
git-new <tosanesi471@gmail.com>
2024-05-23 17:31:38 -0700

    updated

--- sg.tf
+++ sg.tf
@@ -35,3 +35,10 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
   cidr_ipv6         = "::/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
+
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv6         = "::/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
